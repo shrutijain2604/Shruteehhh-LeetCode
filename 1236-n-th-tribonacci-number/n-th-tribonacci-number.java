f57a793fc2c918eb47707dfe8489901a1fac0cf2
Shruti Jain <115571771+shrutijain2604@users.noreply.github.com>
2024-04-24 21:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (84.21%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -4,13 +4,13 @@ class Solution {
 
         int[] dp = {0,1,1};
 
-        for (int i = 3; i <= n; ++i) {
-        final int next = dp[0] + dp[1] + dp[2];
-        dp[0] = dp[1];
-        dp[1] = dp[2];
-        dp[2] = next;
-    }
+        for(int i=3;i<=n;i++){
+            final int next = dp[0]+dp[1]+dp[2];
+            dp[0] =dp[1];
+            dp[1] = dp[2];
+            dp[2] = next;
+        }
 
-    return dp[2];
+        return dp[2];
     }
 }
