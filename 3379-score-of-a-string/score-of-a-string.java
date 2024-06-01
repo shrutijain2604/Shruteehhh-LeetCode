8a1e4777f59dfee59ea3211f9fb8ac25d3e5d5a7
Shruti Jain <115571771+shrutijain2604@users.noreply.github.com>
2024-06-01 13:51:10 +0530

    Time: 1 ms (99.87%) | Memory: 41.9 MB (60.00%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -2,9 +2,14 @@ class Solution {
     public int scoreOfString(String s) {
         int total =0;
         for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                continue;
+            }
             total += Math.abs(s.charAt(i) - s.charAt(i+1));
         }
 
         return total;
     }
 }
+
+
