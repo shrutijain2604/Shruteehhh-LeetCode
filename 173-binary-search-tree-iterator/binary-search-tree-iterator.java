d7feaa07e240b44bb71d041005ccf2068483c661
Shruti Jain <115571771+shrutijain2604@users.noreply.github.com>
2024-09-03 20:31:47 +0530

    Time: 16 ms (99.74%) | Memory: 48.5 MB (6.31%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int index =0;
+    public BSTIterator(TreeNode root) {
+        inOrder(root);
+    }
+    
+    public int next() {
+        return list.get(index++);
+       
+    }
+    
+    public boolean hasNext() {
+        if(index==list.size()){
+            return false;
+        }
+        return true;
+    }
+
+    public void inOrder(TreeNode root){
+        if(root!=null){
+           if(root.left!=null){
+            inOrder(root.left);
+           }
+           list.add(root.val);
+           if(root.right!=null){
+            inOrder(root.right);
+           }
+        }    
+    }
+}
