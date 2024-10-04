ffe023df82b0ad5ebba4209690ce29d4ab1c8df5
Shruti Jain <115571771+shrutijain2604@users.noreply.github.com>
2024-10-04 15:44:28 +0530

    Time: 16 ms (61.21%) | Memory: 57.8 MB (39.29%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        long ans = 0;
+        int i = 0;
+        int j = skill.length - 1;
+        int check = skill[i] + skill[j];
+        while (i < j) {
+            if (skill[i] + skill[j] != check) {
+                return -1;
+            }
+            ans += skill[i] * skill[j];
+            i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
