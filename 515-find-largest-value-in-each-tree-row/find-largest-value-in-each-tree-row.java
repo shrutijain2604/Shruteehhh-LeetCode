99fa243709d87d256f9c8286929fb4f454d1d94b
Shruti Jain <115571771+shrutijain2604@users.noreply.github.com>
2024-12-25 18:59:12 +0530

    Time: 1 ms (94.48%) | Memory: 45.3 MB (78.70%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -1,39 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    List<Integer> ans;
+
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+        ans = new ArrayList<>();
+        dfs(root, 0);
+        return ans;
+    }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            int maxVal = Integer.MIN_VALUE;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                maxVal = Math.max(maxVal, node.val);
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+    private void dfs(TreeNode node, int l) {
+        if (node != null) {
+            int v = node.val;
+            if (l == ans.size()) {
+                ans.add(v);
+            } else {
+                ans.set(l, Math.max(ans.get(l), v));
             }
-            result.add(maxVal);
+            dfs(node.left, l + 1);
+            dfs(node.right, l + 1);
         }
-        return result;
     }
 }
